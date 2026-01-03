a970ee8a4d9e8e350b83371a5953e4d79db8fa82
Wasi-Muzammil <wasimuzammil2005@gmail.com>
2026-01-03 15:18:44 +0500

    PS:06 ,Solution

---
+++ week 1/problem_06.py
@@ -0,0 +1,20 @@
+# Challenge 6: Tower of Hanoi Algorithm
+
+
+def towerOfHanoi(N : int, from_rod : str, to_rod : str, aux_rod : str) -> None:
+    """ Complexity Analysis
+        Approach : Optimal Approach
+        Time Complexity : O(2^n) -> Exponential
+        Space Complexity : O(n) -> Linear
+    """
+    if N == 1:
+        print(f"Disk 1 moved from {from_rod} to {to_rod}")
+        return
+    
+    towerOfHanoi(N - 1, from_rod, aux_rod, to_rod)
+    print(f"Disk {N} moved from {from_rod} to {to_rod}")
+    towerOfHanoi(N - 1, aux_rod, to_rod, from_rod)
+
+
+N = 4
+towerOfHanoi(N, "A", 'C', 'B')
